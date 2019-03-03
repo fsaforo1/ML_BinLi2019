75fa4138227257813011c03e21ace4536c634d37
fsaforo1 <frankforo52@gmail.com>
2019-03-03 08:37:05 -0600

    Remove unneccessary info

--- Code/models/API_Spotify_Genius.R
+++ Code/models/API_Spotify_Genius.R
@@ -1,12 +1,12 @@
-Sys.setenv(SPOTIFY_CLIENT_ID = 'db9cfbe1f69f45d1a4bd641463a6c09e')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'cf422bc7e56a4d82a075c2ad60aa9672')
+Sys.setenv(SPOTIFY_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 access_token <- get_spotify_access_token()
 library(spotifyr)
 #beatles <- get_artist_audio_features('the beatles')
 
 
 # https://github.com/ewenme/geniusr
-Sys.setenv(GENIUS_API_TOKEN = '3YxcxGs0Y-acFzCzoJi8LeK8HeKRbW7yk54qVeLhUh7YktgxDrFf5Vvgk-YfbNJm')
+Sys.setenv(GENIUS_API_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 genius_token()
 
 library(geniusr)
